1b9d282a3713b4972168c21337abc3d384a705ff
pec0ra <heraclez@hotmail.com>
2016-09-30 13:54:40 +0200

    Add licence headers

--- app/src/main/java/ch/pec0ra/mobilityratecalculator/DistanceCalculator.java
+++ app/src/main/java/ch/pec0ra/mobilityratecalculator/DistanceCalculator.java
@@ -1,3 +1,22 @@
+/*
+ *     Copyright (C) 2016 Basile
+ *
+ *     This file is part of Mobility Rate Calculator.
+ *
+ *     Mobility Rate Calculator is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     Foobar is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package ch.pec0ra.mobilityratecalculator;
 
 import android.content.Context;
@@ -18,9 +37,6 @@ import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 
-/**
- * Created by basile on 26.09.16.
- */
 class DistanceCalculator {
 
     private final String from;
